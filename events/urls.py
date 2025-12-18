a6b3f03f51d6f5a82e2518465f33f87adc547679
Dalienst Owino <oduordalienst@gmail.com>
2025-12-19 00:30:07 +0300

    fix-event-detail-error

--- events/urls.py
+++ events/urls.py
@@ -7,7 +7,7 @@ app_name = "events"
 urlpatterns = [
     path("", EventListCreateView.as_view(), name="event-list-create"),
     path(
-        "<str:event_code>",
+        "<str:event_code>/",
         EventRetrieveUpdateDestroyView.as_view(),
         name="event-retrieve-update-destroy",
     ),
